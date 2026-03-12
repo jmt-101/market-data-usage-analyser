cce7c913d8414219966d6ae39ed826875c56670d
Jack <jacktatlock@hotmail.co.uk>
2026-03-12 15:32:38 +0000

    Add data cleaning loop and analysis functions

--- src/analyse_usage.py
+++ src/analyse_usage.py
@@ -12,7 +12,38 @@ df.columns=[
     "bytes",
     "entitlement"
 ]
+
+#Cleaning the data file to make it consistent
+for col in ["user_id", "service", "ric", "entitlement"]:
+    df[col] = df[col].str.strip().str.upper()
+
 print ("Rows:",len(df))
 print ("Columns:",len(df.columns))
 print ("Column names:",df.columns.tolist())
 print (df.head(5))
+
+#Total bytes per user
+bytes_per_user=(
+    df.groupby("user_id") ["bytes"]
+    .sum()
+    .sort_values(ascending=False)
+)
+
+print ("\nTotal bytes per user")
+print (bytes_per_user)
+
+#Usage by Service
+usage_per_service=(
+    df.groupby("service")["msg_count"]
+    .sum()
+    .sort_values(ascending=False)
+)
+print ("\nTotal count per service")
+print (usage_per_service)
+
+#Find enteries for users starting with "Akh"
+userid_starting_with_akh=(
+    df[ df["user_id"].str.lower().str.startswith("akh") ]
+)
+print ("\nUsers ID's that start with Akh")
+print (userid_starting_with_akh)
